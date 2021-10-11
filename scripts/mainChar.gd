5de3a5ab1b5f7a1d33d4b356781c771d9cb7e53b
BathrobeWizard-DD <BathrobeWizard7@gmail.com>
2021-10-11 15:19:52 -0400

    added jumping state
    -now using _input for input handling
    -now has jump state

--- scripts/mainChar.gd
+++ scripts/mainChar.gd
@@ -10,13 +10,16 @@ const RUN_SPEED_MAX = 300
 const JUMP_SPEED = -300
 var velocity = Vector2()
 
-var isJumping
-var isCrouching
+#var isJumping
+#var isCrouching
 
 var characterState 
-onready var runningState = {"get_input": funcref(self,"running_input"), "ready_state": funcref(self, "ready_running")}
-onready var crouchingState = {"get_input": funcref(self,"running_input"), "ready_state": funcref(self, "ready_crouching")}
-
+onready var runningState = {"get_input": funcref(self,"input_running"), "ready_state": funcref(self, "ready_running"),
+							 "process_state": funcref(self, "process_running"), "get_state": funcref(self, "state_running")}
+onready var crouchingState = {"get_input": funcref(self,"input_jumping"), "ready_state": funcref(self, "ready_crouching"),
+							 "process_state": funcref(self, "process_crouching"), "get_state": funcref(self, "state_crouching")}
+onready var jumpingState = {"get_input": funcref(self,"input_jumping"), "ready_state": funcref(self, "ready_jumping"),
+							 "process_state": funcref(self, "process_jumping"), "get_state": funcref(self, "state_jumping")}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	switch_state(runningState)
@@ -41,32 +44,14 @@ func moveLeftOrRight(directionMultiplier):
 	velocity.x += directionMultiplier * (abs(velocity.x) + RUN_ACCEL)
 	velocity.x = clamp(velocity.x, -RUN_SPEED_MAX, RUN_SPEED_MAX)
 
-func get_input():
-#	velocity.x = 0
-	var moveRight = Input.is_action_pressed("moveRight")
-	var moveLeft = Input.is_action_pressed("moveLeft")
-	var jumpUp = Input.is_action_just_pressed("jumpUp")
-	
-	if jumpUp and is_on_floor():
-		isJumping = true
-		velocity.y = JUMP_SPEED
-	
-	isCrouching = Input.is_action_pressed("crouchDown")
-	
-	var directionHorizontal = 0
-	if moveRight:
-		directionHorizontal += 1
-	if moveLeft:
-		directionHorizontal -= 1
-#	moveLeftOrRight(directionHorizontal)
+func _input(event):
+	character_function_args("get_input", [event])
 
 func _physics_process(delta):
-	character_function("get_input")
+	character_function("process_state")
 	
 	velocity.x = RUN_SPEED_MAX
 	velocity.y += GRAVITY * delta
-	if isJumping and is_on_floor():
-		isJumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -79,19 +64,42 @@ func ready_running():
 	$defaultCollisionShape.set_disabled(false)
 	$AnimatedSprite.set_animation("default")
 
-func running_input():
+func ready_crouching():
+	$defaultCollisionShape.set_disabled(true)
+	$AnimatedSprite.set_animation("crouch")
+
+func ready_jumping():
+	self.position.y -= 2
+	velocity.y = JUMP_SPEED
+
+func process_running():
+	pass
+
+func process_crouching():
+	pass
+
+func process_jumping():
+	if is_on_floor():
+		switch_state(runningState)
+
+func state_running():
+	return "running"
+
+func state_crouching():
+	return "crouching"
+
+func state_jumping():
+	return "jumping"
+
+func input_running(event):
 #	velocity.x = 0
-	var moveRight = Input.is_action_pressed("moveRight")
-	var moveLeft = Input.is_action_pressed("moveLeft")
-	var jumpUp = Input.is_action_just_pressed("jumpUp")
-	
-	if jumpUp and is_on_floor():
-		isJumping = true
-		velocity.y = JUMP_SPEED
-	
-	if Input.is_action_just_pressed("crouchDown"):
+	var moveRight = event.is_action_pressed("moveRight")
+	var moveLeft = event.is_action_pressed("moveLeft")
+	if event.is_action_pressed("jumpUp") && is_on_floor():
+		switch_state(jumpingState)
+	if event.is_action_pressed("crouchDown"):
 		switch_state(crouchingState)
-	if Input.is_action_just_released("crouchDown"):
+	if event.is_action_released("crouchDown"):
 		switch_state(runningState)
 	
 	var directionHorizontal = 0
@@ -101,9 +109,21 @@ func running_input():
 		directionHorizontal -= 1
 #	moveLeftOrRight(directionHorizontal)
 
-func ready_crouching():
-	$defaultCollisionShape.set_disabled(true)
-	$AnimatedSprite.set_animation("crouch")
+func input_jumping(event):
+	var moveRight = event.is_action_pressed("moveRight")
+	var moveLeft = event.is_action_pressed("moveLeft")
+	
+	if event.is_action_pressed("crouchDown"):
+		switch_state(crouchingState)
+	if event.is_action_released("crouchDown"):
+		switch_state(runningState)
+	
+	var directionHorizontal = 0
+	if moveRight:
+		directionHorizontal += 1
+	if moveLeft:
+		directionHorizontal -= 1
+#	moveLeftOrRight(directionHorizontal)
 
 func _on_worldWrapperThing_body_entered(body):
 	if (body.get_name() == "mainChar"):

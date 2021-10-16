464526c7265465ca889262b10cab2e32c996e188
BathrobeWizard-DD <73605462+BathrobeWizard-DD@users.noreply.github.com>
2021-10-16 10:09:50 -0400

    Revert "Revert "Mp jp pause menu""

---
+++ scripts/pauseMenu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+func _on_Resume_pressed():
+	visible = false
+	var countdownNode = get_node("../unpauseCountdown")
+	countdownNode.beginTimer()
+	yield(countdownNode, "timerFinished")
+	set_pause(false)
+
+func _on_mainMenu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://scenes/menus/startMenu.tscn")
+
+func set_pause(val):
+	get_tree().paused = val
+	visible = val
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Resume.emit_signal("pressed")
+#	set_pause(false)
+
+func _input(event):
+	if event.is_action_pressed("pauseGame"):
+		var countdownNode = get_node("../unpauseCountdown")
+		if (not countdownNode.isActive):
+			set_pause(true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+

137b93dde0857c055cfc68ca9faf333d584cd23c
BathrobeWizard-DD <BathrobeWizard7@gmail.com>
2021-10-16 22:35:41 -0400

    Black hole logic

--- scripts/Blackhole.gd
+++ scripts/Blackhole.gd
@@ -1,2 +1,23 @@
 extends Node2D
 
+export var blackholeSize = 10
+export var growthRate = 1
+var innerCircleColor = Color( 1, 1, 1, 1)
+var outerCircleColor = Color( 0, 0, 0, 1)
+signal mainCharEntered()
+
+func _ready():
+	get_node("Area2D").position = position + Vector2(-20, -300)
+
+func _draw():
+	draw_circle(position + Vector2(-20, -300), min(blackholeSize * 1.1, blackholeSize + 20), innerCircleColor)
+	draw_circle(position + Vector2(-20, -300), blackholeSize, outerCircleColor)
+
+func _process(delta):
+	blackholeSize += growthRate
+	update()
+	get_node("Area2D/CollisionShape2D").shape.radius = min(blackholeSize * 1.1, blackholeSize + 20)
+
+func _on_Area2D_body_entered(body):
+	if body.name == "mainChar":
+		emit_signal("mainCharEntered")
--- scripts/PlayerSprite.gd
+++ scripts/PlayerSprite.gd
@@ -11,37 +11,37 @@ var is_falling = false
 var old_y_vel := 0.0
 
 func skate(current_speed: int, max_speed, min_speed):
-    is_falling = false
-    is_jumping = false
-    if player.current_animation != "Land":
-        player.playback_speed = range_lerp(current_speed, max_speed, min_speed, 2, 0.75)
-        player.play("Skate")
+	is_falling = false
+	is_jumping = false
+	if player.current_animation != "Land":
+		player.playback_speed = range_lerp(current_speed, max_speed, min_speed, 2, 0.75)
+		player.play("Skate")
 
 
 func crouch():
-    is_falling = false
-    is_jumping = false
-    player.play("Crouch")
+	is_falling = false
+	is_jumping = false
+	player.play("Crouch")
 
 
 func jump(y_vel: float=0.0):
-    if y_vel < end_apex_velocity and y_vel > start_apex_velocity:
-        print("apex")
-        player.play("Apex")
-    elif y_vel <= start_apex_velocity and !is_jumping:
-        print("rising")
-        is_jumping = true
-        player.play("Jump")
-    elif y_vel >= end_apex_velocity and !is_falling:
-        is_falling = true
-        print("falling")
-        player.play("Fall")
-        
+	if y_vel < end_apex_velocity and y_vel > start_apex_velocity:
+		#print("apex")
+		player.play("Apex")
+	elif y_vel <= start_apex_velocity and !is_jumping:
+		#print("rising")
+		is_jumping = true
+		player.play("Jump")
+	elif y_vel >= end_apex_velocity and !is_falling:
+		is_falling = true
+		#print("falling")
+		player.play("Fall")
+		
 
 func charge():
-    is_falling = false
-    is_jumping = false
-    player.play("Charge")
+	is_falling = false
+	is_jumping = false
+	player.play("Charge")
 
 func land():
-    player.play("Land")
+	player.play("Land")
